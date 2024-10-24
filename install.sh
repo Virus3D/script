4b729ca20aef47b738a41474a9240cf1d8e011fa
Virus3D <viisto@yandex.ru>
2024-10-24 19:34:21 +0700

    add Node

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ fi
 # Установка php
 PHP_VERSION="8.2"
 sudo add-apt-repository ppa:ondrej/php
-sudo apt-get install -y php$PHP_VERSION-fpm php$PHP_VERSION-mbstring php$PHP_VERSION-bcmath php$PHP_VERSION-xml php$PHP_VERSION-mysql php$PHP_VERSION-sqlite3 php$PHP_VERSION-common php$PHP_VERSION-gd php$PHP_VERSION-bz2 php$PHP_VERSION-cgi php$PHP_VERSION-cli php$PHP_VERSION-curl php$PHP_VERSION-zip php$PHP_VERSION-gd php$PHP_VERSION-curl php$PHP_VERSION-xdebug 
+sudo apt-get install -y php$PHP_VERSION-fpm php$PHP_VERSION-mbstring php$PHP_VERSION-bcmath php$PHP_VERSION-xml php$PHP_VERSION-mysql php$PHP_VERSION-sqlite3 php$PHP_VERSION-common php$PHP_VERSION-gd php$PHP_VERSION-bz2 php$PHP_VERSION-cgi php$PHP_VERSION-cli php$PHP_VERSION-curl php$PHP_VERSION-zip php$PHP_VERSION-gd php$PHP_VERSION-curl php$PHP_VERSION-xdebug php$PHP_VERSION-intl 
 
 # Включение вывода ошибок
 PHP_INI="/etc/php/$PHP_VERSION/fpm/php.ini"
@@ -56,4 +56,9 @@ sudo systemctl restart php$PHP_VERSION-fpm
 # Установка git
 sudo apt-get install -y git
 
+# Установко node.js
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+nvm install node
